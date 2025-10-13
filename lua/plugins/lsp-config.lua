4a0b92836fb24d812b923a545a0df2bfb16153bf
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2025-10-13 17:45:34 +0530

    lsp fix3

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,6 +17,10 @@ return {
     config = function()
       vim.lsp.enable("ts_ls");
       vim.lsp.enable("lua_ls");
+
+      vim.keymap.set('n' , 'K' , vim.lsp.buf.hover , {})
+      vim.keymap.set('n' , 'gd' , vim.lsp.buf.definition , {})
+      vim.keymap.set({'n' , 'v'} , '<leader>ca' , vim.lsp.buf.code_action , {})
     end
   }
 }
