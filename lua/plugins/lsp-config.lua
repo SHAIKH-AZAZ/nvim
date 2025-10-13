b9d319b23a25f7dca339b869a9406b2046a3fc44
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2025-10-13 16:52:48 +0530

    lsp fix2

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,7 +15,8 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      vim.lsp.enable("ts_ls")  
+      vim.lsp.enable("ts_ls");
+      vim.lsp.enable("lua_ls");
     end
   }
 }
