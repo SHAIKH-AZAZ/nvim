b21db2b636f6d8af8ce9d281328ec4da9f63f951
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2025-10-16 21:47:40 +0530

    lsp setting 2

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -30,7 +30,10 @@ return {
 	  -- Keymaps for LSP
 	  vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 	  vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-	  vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-  end, 
+	  vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {
+        desc = "LSP code action , suggested actions"
+
+      })
+  end,
   },
 }
