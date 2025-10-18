287588b5823b9e89b2fa43c987567bc725af5727
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2025-10-19 00:13:20 +0530

    fix

--- lua/plugins/devicons.lua
+++ lua/plugins/devicons.lua
@@ -1,21 +1,22 @@
-return {
-	"nvim-tree/nvim-web-devicons",
-	lazy = true, -- load only when needed
-	opts = {
-		color_icons = true,
-		default = true,
-		strict = true,
-		override = {
 
-				icon = "",
-				color = "#428850",
-				cterm_color = "65",
-				name = "Zsh",
-			},
-		},
-	},
-	config = function(_, opts)
-		require("nvim-web-devicons").setup(opts)
-	end,
+return {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true, -- load only when needed
+    opts = {
+        color_icons = true,
+        default = true,
+        strict = true,
+        override = {
+            zsh = {        -- you missed the key for override!
+                icon = "",
+                color = "#428850",
+                cterm_color = "65",
+                name = "Zsh",
+            },
+        },
+    },
+    config = function(_, opts)
+        require("nvim-web-devicons").setup(opts)
+    end,
 }
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,71 +1,111 @@
-
-return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  dependencies = {
-    -- Modern maintained rainbow plugin
-    "HiPhish/rainbow-delimiters.nvim",
-  },
-  config = function()
-    local ts_configs = require("nvim-treesitter.configs")
-
-    ts_configs.setup({
-      ensure_installed = {
-        "lua",
-        "javascript",
-        "typescript",
-        "python",
-        "html",
-        "css",
-        "json",
-        "bash",
-        "vim",
-        "markdown",
-      },
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false, -- safer
-      },
-
-      indent = {
-        enable = true,
-      },
-
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
+return { 
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      -- Modern maintained rainbow plugin
+      "HiPhish/rainbow-delimiters.nvim",
+    },
+    config = function()
+      local ts_configs =
+      require("nvim-treesitter.configs"), 
+      ts_configs.setup({
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "go",
+          "yaml",
+          "html",
+          "css",
+          "python",
+          "http",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "svelte",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+          "vimdoc",
+          "c",
+          "java",
+          "rust",
+          "ron",
         },
-      },
-    })
+        auto_install = true,
 
-    -- Rainbow delimiters (replaces old ts-rainbow)
-    local rainbow_delimiters = require("rainbow-delimiters")
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false, -- safer
+        },
 
-    vim.g.rainbow_delimiters = {
-      strategy = {
-        [""] = rainbow_delimiters.strategy["global"],
-        vim = rainbow_delimiters.strategy["local"],
-      },
-      query = {
-        [""] = "rainbow-delimiters",
-        lua = "rainbow-blocks",
-      },
-      highlight = {
-        "RainbowDelimiterRed",
-        "RainbowDelimiterYellow",
-        "RainbowDelimiterBlue",
-        "RainbowDelimiterOrange",
-        "RainbowDelimiterGreen",
-        "RainbowDelimiterViolet",
-        "RainbowDelimiterCyan",
-      },
-    }
-  end,
+        indent = {
+          enable = true,
+        },
+
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+          },
+        },
+      })
+
+      -- Rainbow delimiters (replaces old ts-rainbow)
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      }
+    end,
+  },
+    -- NOTE: js,ts,jsx,tsx Auto Close Tags
+    {
+        "windwp/nvim-ts-autotag",
+        enabled = true,
+        ft = { "html", "xml", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte" },
+        config = function()
+            -- Independent nvim-ts-autotag setup
+            require("nvim-ts-autotag").setup({
+                opts = {
+                    enable_close = true,           -- Auto-close tags
+                    enable_rename = true,          -- Auto-rename pairs
+                    enable_close_on_slash = false, -- Disable auto-close on trailing `</`
+                },
+                per_filetype = {
+                    ["html"] = {
+                        enable_close = true, -- Disable auto-closing for HTML
+                    },
+                    ["typescriptreact"] = {
+                        enable_close = true, -- Explicitly enable auto-closing (optional, defaults to `true`)
+                    },
+                },
+            })
+        end,
+    },
 }
-
