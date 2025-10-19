a7eac8370547351c66e59d73ff6b0bf2d6c9f352
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2025-10-19 11:18:46 +0530

    mason fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,49 +1,67 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-          "pynvim",
-          "pyright"
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"mason.nvim",
-			{ "mason-org/mason-lspconfig.nvim", config = function() end },
-		},
-      
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			vim.lsp.config("lua_ls", {})
-      vim.lsp.config("pynvim" , {})
-			vim.lsp.config("ts_ls", { capabilities = capabilities })
-			vim.lsp.config("tsserver", { capabilities = capabilities })
-      vim.lsp.config("pyright" , {})
-      vim.lsp.enable("pyright")
-			vim.lsp.enable("tsserver")
-      vim.lsp.enable("pynvim")
-			vim.lsp.enable("lua_ls")
-			vim.lsp.enable("ts_ls")
+    "williamboman/mason.nvim",
+    lazy = false,
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "neovim/nvim-lspconfig",
+        -- "saghen/blink.cmp",
+    },
+    config = function()
+        -- import mason and mason_lspconfig
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
-			-- Keymaps for LSP
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {
-				desc = "LSP code action , suggested actions",
-			})
-		end,
-	},
+        -- NOTE: Moved these local imports below back to lspconfig.lua due to mason depracated handlers
+
+        -- local lspconfig = require("lspconfig")
+        -- local cmp_nvim_lsp = require("cmp_nvim_lsp")             -- import cmp-nvim-lsp plugin
+        -- local capabilities = cmp_nvim_lsp.default_capabilities() -- used to enable autocompletion (assign to every lsp server config)
+
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
+
+        mason_lspconfig.setup({
+            automatic_enable = false,
+            -- servers for mason to install
+            ensure_installed = {
+                "lua_ls",
+                "ts_ls", -- currently using a ts plugin
+                "html",
+                "cssls",
+                "tailwindcss",
+                "gopls",
+                "angularls",
+                "emmet_ls",
+                "emmet_language_server",
+                -- "eslint",
+                "marksman",
+            },
+        })
+
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua",   -- lua formatter
+                "isort",    -- python formatter
+                "pylint",
+                "clangd",
+                "denols",
+                -- { 'eslint_d', version = '13.1.2' },
+            },
+
+            -- NOTE: mason BREAKING Change! Removed setup_handlers
+            -- moved lsp configuration settings back into lspconfig.lua file
+        })
+    end,
 }
