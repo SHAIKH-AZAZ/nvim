6b24493d0ad6dd5a7ddd9d1bbbd68c7df340d7c4
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2025-10-23 11:28:45 +0530

    lsp configs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,6 +14,10 @@ return {
 					"lua_ls",
 					"ts_ls",
 					"pyright",
+					"html",
+					"cssls",
+					"tailwindcss",
+					"emmet_ls",
 				},
 			})
 		end,
@@ -24,15 +28,64 @@ return {
 			-- Get capabilities from blink.cmp
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			-- Setup language servers using new Neovim 0.11 API
-			local servers = { "lua_ls", "ts_ls", "pyright" }
+			-- TypeScript/JavaScript with React support
+			vim.lsp.config.ts_ls = {
+				capabilities = capabilities,
+				filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+				settings = {
+					typescript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayFunctionParameterTypeHints = true,
+						},
+					},
+					javascript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayFunctionParameterTypeHints = true,
+						},
+					},
+				},
+			}
+			vim.lsp.enable("ts_ls")
 
-			for _, server in ipairs(servers) do
-				vim.lsp.config[server] = {
-					capabilities = capabilities,
-				}
-				vim.lsp.enable(server)
-			end
+			-- Tailwind CSS
+			vim.lsp.config.tailwindcss = {
+				capabilities = capabilities,
+				filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+			}
+			vim.lsp.enable("tailwindcss")
+
+			-- HTML
+			vim.lsp.config.html = {
+				capabilities = capabilities,
+			}
+			vim.lsp.enable("html")
+
+			-- CSS
+			vim.lsp.config.cssls = {
+				capabilities = capabilities,
+			}
+			vim.lsp.enable("cssls")
+
+			-- Emmet for HTML/JSX
+			vim.lsp.config.emmet_ls = {
+				capabilities = capabilities,
+				filetypes = { "html", "css", "javascriptreact", "typescriptreact" },
+			}
+			vim.lsp.enable("emmet_ls")
+
+			-- Lua
+			vim.lsp.config.lua_ls = {
+				capabilities = capabilities,
+			}
+			vim.lsp.enable("lua_ls")
+
+			-- Python
+			vim.lsp.config.pyright = {
+				capabilities = capabilities,
+			}
+			vim.lsp.enable("pyright")
 
 			-- Keymaps for LSP
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
